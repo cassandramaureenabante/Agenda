e7933f48e421e12ecf6307cf57f6fda08aae9a43
cassandramaureenabante <cassandramaureenabante@gmail.com>
2018-03-18 23:51:05 -0700

    first commit

---
+++ js/agenda.js
@@ -0,0 +1,31 @@
+function  signup(){
+	
+	var name 					= document.getElementById('name');
+	var address 				= document.getElementById('address');
+	var exampleInputEmail1 		= document.getElementById('exampleInputEmail1');
+	var exampleInputPassword1	= document.getElementById('exampleInputPassword1');
+	
+	
+	localStorage.setItem('Name',name.value);
+	localStorage.setItem('Address',address.value);
+	localStorage.setItem('Email',exampleInputEmail1.value);
+	localStorage.setItem('Password',exampleInputPassword1.value);
+	alert('Record Saved');
+	name.value = address.value = exampleInputEmail1.value = exampleInputPassword1.value  = '';
+	
+	
+	
+	
+}
+
+function login(){
+	var exampleInputEmail1 		= document.getElementById('exampleInputEmail1');
+	var exampleInputPassword1	= document.getElementById('exampleInputPassword1');
+	if(document.getElementById('exampleInputEmail1').value == localStorage.Email && exampleInputPassword1.value == localStorage.Password){
+		location.href='home.html';
+	}else{
+		alert('Invalid Username/Password');
+	}
+
+	
+}
